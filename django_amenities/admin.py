d3b763cb2d6e72c1f19b9638242f5dc556cfb216
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-01-15 13:46:02 +0100

    Fix missing comma in admin list_display

--- django_amenities/admin.py
+++ django_amenities/admin.py
@@ -6,7 +6,7 @@ from .models import Amenity
 class AmenityAdmin(admin.ModelAdmin):
     date_hierarchy = 'last_update'
     search_fields = ('name', 'city', 'postcode')
-    list_display = ('name', 'street', 'housenumber', 'postcode' 'city',)
+    list_display = ('name', 'street', 'housenumber', 'postcode', 'city',)
 
 
 admin.site.register(Amenity, AmenityAdmin)
