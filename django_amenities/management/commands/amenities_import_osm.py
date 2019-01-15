c97b08e7f3672a0c381256fc629634e2d01931d3
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-01-15 22:49:35 +0100

    Add update option to importer

--- django_amenities/management/commands/amenities_import_osm.py
+++ django_amenities/management/commands/amenities_import_osm.py
@@ -19,31 +19,46 @@ class Command(BaseCommand):
     help = "Switch venue requests"
 
     def add_arguments(self, parser):
+        parser.add_argument('command', type=str)
         parser.add_argument('filename', type=str)
         parser.add_argument('skip', type=int, default=0, nargs='?')
 
     def handle(self, *args, **options):
         translation.activate(settings.LANGUAGE_CODE)
 
-        Amenity.objects.all().delete()
-        self.stdout.write('Starting import of {filename} at {skip}'.format(
+        self.stdout.write('Starting {command} of {filename} at {skip}'.format(
             **options
         ))
+        command = options['command']
         batch_size = 1000
         skip = options['skip'] * batch_size
-        objs = self.get_amenities(options['filename'], start=skip)
 
-        i = -1
-        while True:
-            i += 1
-            self.stdout.write(str(i), ending='\r')
-            batch = list(islice(objs, batch_size))
-            if not batch:
-                break
-            Amenity.objects.bulk_create(batch, batch_size)
+        if command == 'insert':
+            objs = self.get_amenities(
+                options['filename'], as_objects=True, start=skip
+            )
+            i = -1
+            while True:
+                i += 1
+                self.stdout.write(str(i), ending='\r')
+                batch = list(islice(objs, batch_size))
+                if not batch:
+                    break
+                Amenity.objects.bulk_create(batch, batch_size)
+        elif command == 'update':
+            objs = self.get_amenities(
+                options['filename'], as_objects=False, start=skip
+            )
+            for i, amenity in enumerate(objs):
+                if i % 1000 == 0:
+                    self.stdout.write(str(i), ending='\r')
+                Amenity.objects.get_or_create(
+                    osm_id=amenity['osm_id'],
+                    defaults=amenity
+                )
         self.stdout.write('\nDone')
 
-    def get_amenities(self, filename, version=0, start=0):
+    def get_amenities(self, filename, version=0, start=0, as_objects=False):
         basic_tags = (
             'name', 'addr:country', 'addr:street', 'addr:housenumber',
             'addr:postcode', 'addr:city', 'amenity', 'shop'
@@ -51,16 +66,20 @@ class Command(BaseCommand):
         with open(filename, 'rb') as f:
             context = etree.iterparse(f, events=("start",), tag='node')
             for action, elem in islice(context, start, None):
+                data = {
+                    x.attrib['k']: x.attrib['v']
+                    for x in elem.xpath('./tag')
+                }
+                if 'amenity' not in data and 'shop' not in data:
+                    continue
+
                 attrs = elem.attrib
                 last_update = datetime.strptime(
                     attrs['timestamp'], "%Y-%m-%dT%H:%M:%SZ"
                 )
                 last_update = pytz.utc.localize(last_update, is_dst=None)
-                data = {
-                    x.attrib['k']: x.attrib['v']
-                    for x in elem.xpath('./tag')
-                }
-                basic = {k.replace('addr:', ''): v for k, v in data.items() if k in basic_tags}
+                basic = {k.replace('addr:', ''): v for k, v in data.items()
+                         if k in basic_tags}
                 if 'shop' in basic:
                     basic['amenity'] = basic.pop('shop')
                 if 'housenumber' in basic:
@@ -72,11 +91,14 @@ class Command(BaseCommand):
                 if 'country' in basic:
                     basic['country'] = basic['country'][:2]
                 tags = {k: v for k, v in data.items() if k not in basic_tags}
-                yield Amenity(
-                   osm_id=int(attrs['id']),
-                   geo=Point(float(attrs['lon']), float(attrs['lat'])),
-                   last_update=last_update,
-                   version=version,
-                   tags=tags,
-                   **basic
-                )
+                basic.update(dict(
+                    osm_id=int(attrs['id']),
+                    geo=Point(float(attrs['lon']), float(attrs['lat'])),
+                    last_update=last_update,
+                    version=version,
+                    tags=tags,
+                ))
+                if as_objects:
+                    yield Amenity(**basic)
+                else:
+                    yield basic
