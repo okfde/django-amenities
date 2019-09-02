99897cf3876a2b4b455f47b593eff22c83b4a6ca
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-09-02 12:21:06 +0200

    Add admin filter for version

--- django_amenities/admin.py
+++ django_amenities/admin.py
@@ -7,6 +7,7 @@ class AmenityAdmin(admin.ModelAdmin):
     date_hierarchy = 'last_update'
     search_fields = ('name', 'city', 'postcode')
     list_display = ('name', 'street', 'housenumber', 'postcode', 'city',)
+    list_filter = ('version',)
 
 
 admin.site.register(Amenity, AmenityAdmin)
